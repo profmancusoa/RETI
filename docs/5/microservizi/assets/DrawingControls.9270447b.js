542c7e91dc64616fe88a737f27bebc07a1bca672
profmancusoa <profmancusoa@gmail.com>
2023-02-04 17:57:41 +0100

    aggiunto microservizi

---
+++ docs/5/microservizi/assets/DrawingControls.9270447b.js
@@ -0,0 +1,1 @@
+import"./DrawingControls.vue_vue_type_script_setup_true_lang.e0dad4ba.js";import{_ as t}from"./DrawingControls.vue_vue_type_script_setup_true_lang.e0dad4ba.js";import"./index.110ce306.js";export{t as default};
---
+++ docs/5/microservizi/assets/DrawingPreview.733b3d8c.js
@@ -0,0 +1,1 @@
+import"./index.110ce306.js";import{ah as r}from"./index.110ce306.js";export{r as default};
